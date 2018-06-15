7ce26dab21528754447ce1146eedeeb9182717ac
Ethan Xia <sanhust@gmail.com>
2018-06-15 14:32:53 +0800

    SCRIPT: Add vim setup in debian.sh
    Signed-off-by: Ethan Xia <sanhust@gmail.com>

--- script/debian.sh
+++ script/debian.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -
 
-############################################################################### 
+################################################################################
 # Set flags so script is executed in "strict mode"
-############################################################################### 
+################################################################################
 
 set -u # Prevent unset variables
 set -e # Stop on an error
@@ -10,13 +10,48 @@ set -o pipefail # Pipe exit code should be non-zero when a command in it fails
 IFS=$'\t\n' # Stricter IFS settings
 ORIGINAL_IFS=$IFS
 
+################################################################################
+# Install prerequisite packages
+################################################################################
+
 sudo apt-get -y install git
 sudo apt-get -y install vim
 sudo apt-get -y install tmux
 sudo apt-get -y install curl
 
 sudo apt-get -y install python3
-# Install pip to install Python package easily
 sudo apt-get -y install python3-pip
 # Install the Python autocompletion library Jedi
-sudo pip3 install -U jedi
+# sudo path: /usr/local/lib/python3.5/dist-packages/jedi
+# user path: pip3 install --user jedi
+sudo -H pip3 install -U jedi
+
+################################################################################
+# Setup vim
+################################################################################
+
+# Clone vimrc
+mkdir -p $HOME/git
+cd $HOME/git
+if [ ! -d dotfiles ]; then
+	git clone https://github.com/triplekiller/dotfiles.git
+fi
+cp dotfiles/conf/vimrc $HOME/.vimrc
+cd $HOME
+
+# Clone Vundle
+if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
+	git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+fi
+
+# Install vim-instant-markdown plugin
+sudo apt-get -y install xdg-utils curl nodejs-legacy
+sudo apt-get -y install npm
+# /usr/local/lib/node_modules/instant-markdown-d
+sudo npm -g install instant-markdown-d
+
+# Install Vundle plugins
+vim +PluginInstall +qall
+
+# Install clang and clang-format
+sudo apt-get -y install clang clang-format
