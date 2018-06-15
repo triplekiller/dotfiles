f59ff124dc9f5675990f135910687123ac057e6a
Ethan Xia <sanhust@gmail.com>
2018-06-15 14:26:28 +0800

    CMD: Optimize sshfs.sh
    Signed-off-by: Ethan Xia <sanhust@gmail.com>

--- cmd/sshfs.sh
+++ cmd/sshfs.sh
@@ -10,59 +10,71 @@ function can_mount() {
 	fi
 }
 
-function start_sshfs() {
+function sshfs_mount() {
 	can_mount
 	if [ $? -eq 0 ]; then
 		echo "Already mounted, first umount..."
 		exit 1
 	fi
 
-	sshfs -o uid=$(id -u) -o gid=$(id -u) -o sshfs_sync -o reconnect -o \
-		follow_symlinks san@one77.local:/home/san/aosp /home/san/mnt/aosp/ \
+	local mount_dev
+	local mount_dir
 
-	sleep 2
+	mount_dev="$1"
+	mount_dir="$HOME/mnt/"`basename "$mount_dev"`
+	mkdir -p "$mount_dir"
+
+#	sshfs -o uid=$(id -u) -o gid=$(id -u) -o sshfs_sync -o reconnect -o \
+#		follow_symlinks san@one77.local:/home/san/aosp /home/san/mnt/aosp/ \
 
 	sshfs -o uid=$(id -u) -o gid=$(id -u) -o sshfs_sync -o reconnect -o \
-		follow_symlinks san@one77.local:/home/san/git/alp/armada/buildroot-new/out/abox_hub/images \
-		/tftpboot/abox_hub
+		follow_symlinks "$mount_dev" "$mount_dir"
 }
 
-function stop_sshfs() {
+function sshfs_umount() {
 	can_mount
 	if [ $? -eq 1 ]; then
 		echo "Already umounted..."
 		exit 1
 	fi
 
-	fusermount -u /home/san/mnt/aosp
-	fusermount -u /tftpboot/abox_hub
+	local mount_dir
+	mount_dir="$1"
+
+	fusermount -u "$mount_dir"
 }
 
 function print_usage() {
 	cat << EOF
 
 Usage: ${0##*/} [option]
+ ${0##*/} -h
+ ${0##*/} <source> <directory>
+ ${0##*/} -u <directory>
+
 Mount your remote directory using sshfs.
-  -h, --help    display this help and test
-  -m --mount    mount the directory
-  -u, --umount  umount the directory
+
+Options:
+ -h, --help                display this help and exit
+ -m, --mount  <src> <dir>  mount the directory
+ -u, --umount <dir>        umount the directory
 EOF
 
 	exit 1
 }
 
-[ $# -eq 0 ] && print_usage
+[ $# -lt 2 ] && print_usage
 
 GETOPT="$(which getopt)"
-ARGS=$(${GETOPT} -o hmu -l "help,mount,umount" -n "${0##*/}" -- "$@")
+ARGS=$(${GETOPT} -o "hm:u:" -l "help,mount,umount" -n "${0##*/}" -- "$@")
 if [ $? -ne 0 ]; then print_usage; fi;
 eval set -- "$ARGS"
 
 while true; do
 	case "$1" in
 		-h|--help) shift; print_usage;;
-		-m|--mount) shift; start_sshfs;;
-		-u|--umount) shift; stop_sshfs;;
+		-m|--mount) shift; sshfs_mount "$1";;
+		-u|--umount) shift; sshfs_umount "$1";;
 		--) shift; break;;
 		*) echo "Internal error..."; exit 1;;
 	esac
